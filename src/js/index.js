f68213e8880da19425d5cd6d8386c1c21a9f0b96
Vlad Pryimak <prmk.vlad@gmail.com>
2024-07-10 18:09:18 +0300

    Add addaptability for best sellers section

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,24 @@ import 'swiper/css/bundle';
 
 const swiper = new Swiper('.swiper', {
 	// Optional parameters
-	slidesPerView: 3,
+	slidesPerView: 2,
 	loop: true,
-	spaceBetween: 24,
+	spaceBetween: 16,
+
+	breakpoints: {
+		780: {
+			slidesPerView: 3,
+			spaceBetween: 24,
+		}
+	},
 
 	// If we need pagination
 	pagination: {
 		el: '.swiper-pagination',
 		clickable: true,
 	},
+
+
 });
 
 // Мобильная навигация
